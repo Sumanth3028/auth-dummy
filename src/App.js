3855b4638c572b82a80483b17734283ab3e12865
Sumanth3028 <sumanthakula05@gmail.com>
2023-03-06 13:14:15 +0530

    redirecting between pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/HomePage';
 
 function App() {
   return (
-   
+    
     <Layout>
       <Routes>
         <Route path='/' element={<HomePage />} >
@@ -21,7 +21,7 @@ function App() {
         </Route>
       </Routes>
     </Layout>
-   
+    
     
   );
 }
