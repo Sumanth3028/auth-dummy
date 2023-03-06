95007812a6d5a5198309ddb08eea68b2dc0864b8
Sumanth3028 <sumanthakula05@gmail.com>
2023-03-06 14:02:08 +0530

    setting api token

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import { Switch, Route, Routes } from 'react-router-dom';
-
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import { Switch, Route, Routes, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
-    
     <Layout>
       <Routes>
-        <Route path='/' element={<HomePage />} >
-          
-        </Route>
-        <Route path='/auth' element={<AuthPage />}>
-          
-        </Route>
-        <Route path='/profile' element={<UserProfile />}>
-      
-        </Route>
+        <Route path="/" element={<HomePage />}></Route>
+        {!authCtx.isLoggedIn &&<Route path="/auth" element={<AuthPage />}></Route>}
+        {authCtx.isLoggedIn && (
+          <Route path="/profile" element={<UserProfile />}></Route>
+        )}
+        <Route path='*' element={<Navigate to='/'></Navigate>} />
       </Routes>
     </Layout>
-    
-    
   );
 }
 
